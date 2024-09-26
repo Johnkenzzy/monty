ee378714ecb847e56747d011a3735c3ae64416fb
johnkenzzy <johnkenumeh@gmail.com>
2024-09-26 08:28:57 -0700

    Updated header file

--- monty.h
+++ monty.h
@@ -48,6 +48,7 @@ void execute_monty(FILE *file);
 /* Pycodes implementation functions */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 
 /* Helper functions */
