8228f16164cd72aa22486484b95921637f9bb663
johnkenzzy <johnkenumeh@gmail.com>
2024-09-26 06:34:35 -0700

    Called functions on errors

--- main.c
+++ main.c
@@ -14,17 +14,12 @@ int main(int argc, char **argv)
 	FILE *file;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+		usage_error();
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+		file_error(argv[1]);
+
 	execute_monty(file);
 
 	fclose(file);
