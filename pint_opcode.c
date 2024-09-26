e7cd75ce8d024308cd6e2b3571fdf06bb083d30c
johnkenzzy <johnkenumeh@gmail.com>
2024-09-26 08:28:16 -0700

    pint opcode implementation

---
+++ pint_opcode.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pint - prints the value at the top of the stack
+ *
+ * @stack: current top node of stack
+ * @line_number: line in fille where cammand function was called
+ *
+ * Return: nothing
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
