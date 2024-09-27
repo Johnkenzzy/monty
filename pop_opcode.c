cb04fe8f6cae0b8e663434d25407b5b6215603c3
johnkenzzy <johnkenumeh@gmail.com>
2024-09-26 18:48:02 -0700

    Pop opcode implementation

---
+++ pop_opcode.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * pop - removes the element at the top of the stack
+ *
+ * @stack: current top node of stack
+ * @line_number: line in fille where cammand function was called
+ *
+ * Return: nothing
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *stack_top_node;
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	stack_top_node = *stack;
+	*stack = (*stack)->next;
+
+	if (*stack)
+		(*stack)->prev = NULL;
+
+	free(stack_top_node);
+}
