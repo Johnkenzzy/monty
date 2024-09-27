cf13a84fa5ed3c3346d4bf9eec02247506a6e367
johnkenzzy <johnkenumeh@gmail.com>
2024-09-26 19:52:18 -0700

    nop opcode implementation

---
+++ nop_opcode.c
@@ -0,0 +1,16 @@
+#include "Monty.h"
+
+
+/**
+ * nop - does nothing
+ *
+ * @stack: pointer to the top of the stack (unused)
+ * @line_number: line number where the command was called (unused)
+ *
+ * Return: nothing
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
