52fcaa6598e0d4b5ba34d5685b69406f5d47731a
johnkenzzy <johnkenumeh@gmail.com>
2024-09-29 22:02:19 -0700

    Updated headerfile

--- monty.h
+++ monty.h
@@ -8,6 +8,7 @@
 #include <ctype.h>
 
 
+#define MODE 0
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -61,6 +62,8 @@ void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void queue(stack_t **stack, unsigned int line_number);
 
 
 /* Helper functions */
